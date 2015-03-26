af2863c639fe85f77ae6bed428b1cd1a90b4caf8
aditya <dev@chimeracoder.net>
2015-03-26 11:38:27 -0400

    Remove test of fetching Enron corpus

--- enron/enron_test.go
+++ enron/enron_test.go
@@ -5,13 +5,6 @@ import (
 	"testing"
 )
 
-func Test_GetCorpus(t *testing.T) {
-	_, err := EnronCorpus()
-	if err != nil {
-		t.Error(err)
-	}
-}
-
 func Test_Untar(t *testing.T) {
 	err := untar(path.Join(app.UserData(), _CorpusFilename))
 	if err != nil {
